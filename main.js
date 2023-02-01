df04509d1840a5b4b7146c2112b547ce6fd0b0b6
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-02-01 14:39:09 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-// your code here
+//There is a way to make this better but JQuery will make it better
+
+document.getElementById('submit').addEventListener('click', function () {
+  console.log('click');
+
+  var name = document.getElementById('name').value;
+  var text = document.getElementById('message').value;
+
+  var postBox = document.querySelector('.posts');
+
+  var newPostBox = document.createElement('div');
+
+  var newPostTextP = document.createElement('p');
+  var newPostTextNode = document.createTextNode(text);
+newPostTextP.appendChild(newPostTextNode);
+
+  var newPostNameP = document.createElement('p');
+  var newPostNameNode = document.createTextNode('Brought to you by ' + name);
+  newPostNameP.appendChild(newPostNameNode);
+
+
+  var newPostHR = document.createElement('hr');
+
+
+  newPostBox.append(newPostTextP);
+  newPostBox.append(newPostNameP);
+  newPostBox.append(newPostHR);
+
+  postBox.append(newPostBox);
+  // debugger
+
+
+
+
+});
+
+
+var name;
+
+var text;
